7addd9505188b19352c4b19a5c7d4dde41a70143
smaheshkumar <smakeshindia@gmail.com>
2024-04-20 22:46:14 +0400

    Navigation styles updated with routes

--- src/App.js
+++ src/App.js
@@ -2,27 +2,27 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login'
 import Dashboard from './components/Dashboard'
+import Navigation from './components/Navigation';
 import AuthProvider from './hooks/AuthProvider';
 import PrivateRoute from './router/route';
 
 function App() {
   return (
-    <div className="App">
+    <div className="mx-auto max-w-7xl">
       <Router>
+        <Navigation />
         <AuthProvider>
-          <header className="App-header">
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route element={<PrivateRoute />}>
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/" element={<Dashboard />} />
-              </Route>
-              { /** Other routes */}
-            </Routes>
-          </header>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route element={<PrivateRoute />}>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/" element={<Dashboard />} />
+            </Route>
+            { /** Other routes */}
+          </Routes>
         </AuthProvider>
       </Router>
-    </div >
+    </div>
   );
 }
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
     const auth = useAuth();
     const handleSubmitEvent = (e) => {
         e.preventDefault();
-        if(input.username !== '' && input.password !== '') {
+        if (input.username !== '' && input.password !== '') {
             auth.loginAction(input);
             return;
         }
@@ -26,38 +26,69 @@ const Login = () => {
     };
 
     return (
-        <form onSubmit={handleSubmitEvent}>
-            <div className='form_control'>
-                <label htmlFor='user-email'>Email:</label>
-                <input
-                    type='email'
-                    id='user-email'
-                    name='username'
-                    placeholder='example@mail.com'
-                    aria-describedby='user-email'
-                    aria-invalid='false'
-                    onChange={handleInput}
-                />
-                <div id='user-email' className='sr-only'>
-                    Please enter a calid username. It must contain at least 6 characters.
-                </div>
+        <div class="flex min-h-full flex-col justify-center">
+            <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h2 class="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
             </div>
-            <div className='form_control'>
-                <label htmlFor='password'>Password:</label>
-                <input
-                    type='password'
-                    id='password'
-                    name='password'
-                    aria-describedby='user-password'
-                    aria-invalid='false'
-                    onChange={handleInput}
-                />
-                <div id='user-password' className='sr-only'>
-                    your password should be more than 6 character
-                </div>
+
+            <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form class="space-y-6" action="#" method="POST" onSubmit={handleSubmitEvent}>
+                    <div>
+                        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                        <div class="mt-2">
+                            <input
+                                id='user-email'
+                                name='username'
+                                type="email"
+                                placeholder='example@mail.com'
+                                aria-describedby='user-email'
+                                aria-invalid='false'
+                                required
+                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                onChange={handleInput}
+                            />
+                            <div id='user-email' className='sr-only'>
+                                Please enter a calid username. It must contain at least 6 characters.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="flex items-center justify-between">
+                            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                            <div class="text-sm">
+                                <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+                            </div>
+                        </div>
+                        <div class="mt-2">
+                            <input
+                                type='password'
+                                id='password'
+                                name='password'
+                                aria-describedby='user-password'
+                                aria-invalid='false'
+                                onChange={handleInput}
+                                autocomplete="current-password"
+                                required
+                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                            <div id='user-password' className='sr-only'>
+                                your password should be more than 6 character
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <button
+                            type="submit"
+                            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            Sign in
+                        </button>
+                    </div>
+                </form>
             </div>
-            <button className='btn-submit'>Submit</button>
-        </form>
+        </div>
     )
 }
 
